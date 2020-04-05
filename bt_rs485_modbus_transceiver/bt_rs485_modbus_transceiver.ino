431267856c9b17785574cc2523054c6b2420e905
Alexey Zaytsev <alex.rv9rh@gmail.com>
2020-04-05 16:12:05 +0500

    improved request processing

--- bt_rs485_modbus_transceiver/bt_rs485_modbus_transceiver.ino
+++ bt_rs485_modbus_transceiver/bt_rs485_modbus_transceiver.ino
@@ -27,29 +27,51 @@ void loop() {
   // со смарта в комп:
   if (btSerial.available()) {
     
-    byte buf[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    btSerial.readBytes(buf, 10);
+    byte buf[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int bufLength = sizeof(buf);
+    btSerial.readBytes(buf, bufLength);
+	//Serial.println(bufLength);
+	
+	int funcCode = buf[1];
+	int reqLength = 8; // длина команды для большинства функций
+	
+	// команды с кодами 15(0x0F) и 16(0x10) могут быть длиннее 8 байт
+	if ((funcCode == 15) || (funcCode == 16)) {
+		//Serial.println("more bites");
+		
+		// Читаем 7й байт (buf[6]), в нём количество байт идущих далее
+		// и добавляем ещё 2 байта CRC
+		int numNextBytes = buf[6];
+		reqLength = 7 + numNextBytes + 2;
+		
+		// если вдруг запрос получился длиннее буфера (на всякий случай)
+		// то просто шлём весь буфер
+		if (reqLength > bufLength) {
+			reqLength = bufLength;
+		}
+		//Serial.println(reqLength);
+	}
 
-    int reqLength = buf[0];
+    //int reqLength = buf[0];
     //Serial.println(reqLength);
-
-    byte request[reqLength];
+	
+    //byte request[reqLength];
     //digitalWrite(13, HIGH); // включаем передачу
-    for (int i = 0; i < reqLength; i++) {
-      request[i] = buf[i + 1];
+    //for (int i = 0; i < reqLength; i++) {
+      //request[i] = buf[i + 1];
 
       //Serial.print(buf[i + 1], HEX);
       //Serial.print(" ");      
       //rs485.print(buf[i + 1], HEX);
       
       //Serial.write(buf[i + 1]);
-    }
+    //}
     //Serial.println();
     //digitalWrite(13, LOW); // включаем приём
 
     // первый вариант:
     digitalWrite(13, HIGH); // включаем передачу
-    Serial.write(request, reqLength);
+    Serial.write(buf, reqLength);
     delay(10); // может уменьшить?
     digitalWrite(13, LOW); // включаем приём
     //===========================
